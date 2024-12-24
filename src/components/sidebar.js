60e698b5d906cc97bb3cb9b8a1d135b169e2774e
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-12-24 16:28:16 +0530

    sidebar-fix-2

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -68,7 +68,7 @@ export default function Sidebar({ children }) {
                 </div>
             </nav>
 
-            <aside id="logo-sidebar" className={`sm:block ${isOpen ? 'open' : 'hidden'} fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`} aria-label="Sidebar">
+            <aside id="logo-sidebar" className={`sm:block ${isOpen ? 'block' : 'hidden'} fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`} aria-label="Sidebar">
                 <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                     <ul class="space-y-2 font-medium">
                         <li>
