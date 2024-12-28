72181f4ac67eabcee02fe02d883132fe7abde79c
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-12-28 09:15:45 +0530

    sidebar-fixed

--- src/app/concepts/arithmetic-aptitude/page.js
+++ src/app/concepts/arithmetic-aptitude/page.js
@@ -9,16 +9,16 @@ export default function ArithmeticAptitude() {
     return (
         <>
             <div>
-                <h1 className="text-xl font-semibold">Arithmetic Aptitude</h1>
+                <h1 className="text-xl font-semibold px-4 py-2">Arithmetic Aptitude</h1>
 
                 <div className="grid lg:grid-cols-2 grid-cols-1 gap-2 mt-4">
                     {
                         topics.map((item, index) => (
 
-                            <Link key={index} href={`/concepts/arithmetic-aptitude/${item.replaceAll(" ","-").toLowerCase()}`} className="flex gap-2 items-center px-4 py-2 hover:bg-green-50 rounded-md hover:text-green-800 w-full">
+                            <Link key={index} href={`/concepts/arithmetic-aptitude/${item.replaceAll(" ", "-").toLowerCase()}`} className="inline-flex gap-2 items-center px-4 py-2 hover:bg-yellow-50 rounded-md hover:text-green-800 w-full">
                                 <span className="text-yellow-400">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
-                                        <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16">
+                                        <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
                                     </svg>
 
                                 </span>
